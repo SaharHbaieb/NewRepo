f54edf8b51715b298565bb7f88e7402b265e0c33
shbaieb <shbaieb@SAHAR-PC>
2017-12-25 17:58:33 +0100

    eee

---
+++ WindowsFormsApplication1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    class Class1
+    {
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ WindowsFormsApplication1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            {
+
+                {
+                    // arranger
+                    int a = 1;
+                    int b = 2;
+                    // agir
+                    /*  Class1 c1 = new Class1 (); */
+                    int resultat = Class1.Addition(a, b);
+                    // auditer
+                    if (resultat != 3)
+                        Console.WriteLine("Le test a raté");
+                }
+                           }
+              /*  Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1()); */
+            }
+        }
+    } 
